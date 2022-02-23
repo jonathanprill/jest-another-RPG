9841d6affd33e2afa1f992def87f28ff8d951cb1
jonyprill <jonathantprill@gmail.com>
2022-02-23 13:54:58 -0600

    added inquirer and started game logic

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // First, run npm install jest --save-dev.
 // The --save-dev flag tells npm to add Jest to the devDependencies list.
 
+const Game = require('./lib/Game');
+
+new Game().initializeGame();
